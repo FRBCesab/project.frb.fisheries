22b73c524c8c407b95b5c73b36665f9f36238441
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-02 15:04:39 +0100

    Entete new script

---
+++ R/merge_clean_data_files.R
@@ -0,0 +1,8 @@
+#####################################################
+# 2/12/2021
+# Celine Albert, Samuel Charberet, Lola Gilbert
+#
+# merge_clean_data_files.R
+#
+# Script with functions to merge both data files
+#####################################################
