f108dd964ffe8348947c93a50b97cdf2512ce050
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-02 10:14:42 +0100

    Set up script

---
+++ analysis/00_setup.R
@@ -0,0 +1,22 @@
+##############################################
+#02/12/2021
+# Celine Albert, Samuel Charberet, Lola Gilbert
+#
+# 00_setup.R
+#
+# projet FRB CESAB
+# compendium creation and set.up
+##############################################
+
+# compendium creation
+library(rrtools)
+
+rrtools::use_compendium(path = "../project.frb.fisheries/",
+                        open = FALSE)
+
+# set up
+dir.create("data")
+dir.create("outputs")
+dir.create("analysis")
+dir.create("R")
+dir.create("manuscript")
