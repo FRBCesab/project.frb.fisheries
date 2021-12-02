e3668d0feee8e29ac3e257b895392bb65ecfa5c8
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-02 16:33:59 +0100

    Create merge function

--- R/merge_clean_data_files.R
+++ R/merge_clean_data_files.R
@@ -6,3 +6,17 @@
 #
 # Script with functions to merge both data files
 #####################################################
+
+#' @title Merge both files
+#'
+#'
+#'
+#' @return unique tibble to work on
+merge_files <- function(tibble_SAU, tibble_CNP) {
+  data <- tibble_SAU |>
+    # join the two tibbles
+    dplyr::left_join(tibble_CNP, by = "scientific_name") |>
+    dplyr::filter(!is.na(c_mean))
+
+  data
+}
