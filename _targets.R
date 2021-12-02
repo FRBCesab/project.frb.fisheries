b234bc0993d8fb37e5c155bd895dc8562f67527b
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-02 16:58:58 +0100

    First update of target

--- _targets.R
+++ _targets.R
@@ -1,14 +1,24 @@
+#################################################
+
+
+
+#################################################
 # _targets.R file
 
 library(targets)
 # We source all functions contained in all files in the R directory
-lapply(list.files(here::here("R"), recursive = TRUE, full.names = T), source)
+lapply(list.files(here::here("R"),
+                  recursive = TRUE, full.names = T),
+       source)
 
 
 list(
-  tar_target(data_sau, load_SAU()),
-  # reads the Sea Around Us data,  returns a data.frame
-  tar_target(data_ghs, wrangle_ghs_data()),
+  # load and clean both data files
+  tar_target(data_sau, load_SAU()), # Sea Around us data
+  tar_target(data_ghs, wrangle_ghs_data()), # CNP data
+  # update taxo on SAU and data
+  tar_target(SAU_taxo, add_gbif_backbone_taxonomy(data_sau)),
+  tar_target(GHS_taxo, add_gbif_backbone_taxonomy(data_ghs)),
   #read the Global heterotroph stoichiometry data, returns a data.frame
   tar_target(dat_seaghs, the_function_that_merges),
   # merge both datasets
