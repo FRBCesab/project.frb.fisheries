a7823f47c188c6fde20478e4d647f31a4efbb924
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-03 09:52:09 +0100

    Correction conversions

--- R/03_merge_clean_data_files.R
+++ R/03_merge_clean_data_files.R
@@ -33,9 +33,9 @@ format_merged <- function(tibble_merged) {
     dplyr::mutate(# convert % dry mass means into wet weight concentrations
                   # with a 80% water content
                   # unit g/ton
-                  c_g_per_t = (c_mean/5)*1e-2,
-                  n_g_per_t = (n_mean/5)*1e-2,
-                  p_g_per_t = (p_mean/5)*1e-2,
+                  c_g_per_t = (c_mean/5)*1e4,
+                  n_g_per_t = (n_mean/5)*1e4,
+                  p_g_per_t = (p_mean/5)*1e4,
                   # compute equivalent export of C, N, P, in tons
                   C_tonnage = c_g_per_t*landed_value*1e-6,
                   N_tonnage = n_g_per_t*landed_value*1e-6,
