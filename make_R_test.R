c6d2bad38d05fde6b31016f82b05a9eecfb536c6
Samuel Charberet <samuelcharberet@gmail.com>
2021-12-03 14:31:19 +0100

    removed the test

--- make_R_test.R
+++
@@ -1,5 +0,0 @@
-lapply(list.files(here::here("R"), recursive = TRUE, full.names = T), source)
-
-b = wrangling_ghs_data()
-a = add_gbif_backbone_taxonomy(dataframe=ghs_data_aggregated, speciescolumn = "scientific_name")
-
