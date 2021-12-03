fd88e5973b0e3a0053ea2dcf83e5e1e465e55d80
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-03 12:33:50 +0100

    Make up plots

--- R/04_plots.R
+++ R/04_plots.R
@@ -14,8 +14,8 @@ graph_barplot_ton <- function(tibble) {
     ggplot() +
     aes(year, landed_value, fill = scientific_name) +
     geom_bar(stat = "identity")+
-    ylab ("Landed value (tons)") +
-    xlab ("Year")+
+    ylab ("Landed tonnage (tons)") +
+    xlab ("Year") +
     theme(panel.background = element_rect(fill = "white", colour = "white"),
           panel.grid.major = element_line(size = 0.5, linetype = 'solid', colour = "grey95"),
           panel.grid.minor = element_line(size = 0.25, linetype = 'solid', colour = "white"),
@@ -23,8 +23,11 @@ graph_barplot_ton <- function(tibble) {
           axis.text.x = element_text(size=9, angle = 60, hjust=1),
           strip.background=element_rect(colour="grey90", fill="grey95"),
           axis.title.y = element_text(size = 9, face = "bold"),
-          axis.text.y = element_text(size=9))+
-    ghibli::scale_fill_ghibli_d(name = "PonyoMedium", direction = - 1) +
+          axis.text.y = element_text(size=9)) +
+    scale_fill_manual(name = "Species",
+                      values = c("#446590FF", "#2D2A25FF",
+                                 "#534C53FF", "#583B2BFF")) +
+    #ghibli::scale_fill_ghibli_d(name = "PonyoMedium", direction = - 1) +
     facet_wrap(.~ fishing_entity, nrow = 3)
 }
 
@@ -37,12 +40,26 @@ graph_barplot_ton <- function(tibble) {
 #' @return ggplot object
 graphCNP_export <- function(data) {
   data %>%
-    pivot_longer(cols = c(C_tonnage, N_tonnage, P_tonnage),
+    tidyr::pivot_longer(cols = c(C_tonnage, N_tonnage, P_tonnage),
                  names_to = "nut_exp",
                  values_to = "tonnage") %>%
+    dplyr::mutate(nut_exp = case_when(nut_exp == "C_tonnage" ~ "C",
+                                      nut_exp == "N_tonnage" ~ "N",
+                                      nut_exp == "P_tonnage" ~ "P")) %>%
     ggplot() +
     aes(x = year, y = tonnage, fill = nut_exp) +
     geom_col() +
     facet_wrap(~ nut_exp) +
-    ghibli::scale_fill_ghibli_d(name = "MononokeMedium", direction = -1)
+    xlab("Year") + ylab("Total export (tons)") +
+    scale_fill_manual(values = c("#278B9AFF", "#E75B64FF", "#D8AF39FF")) +
+    #ghibli::scale_fill_ghibli_d(name = "MononokeMedium", direction = -1)
+    theme(panel.background = element_rect(fill = "white", colour = "white"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid', colour = "grey95"),
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid', colour = "white"),
+          axis.title.x = element_text(size = 9, face = "bold"),
+          axis.text.x = element_text(size=9, angle = 60, hjust=1),
+          strip.background=element_rect(colour="grey90", fill="grey95"),
+          axis.title.y = element_text(size = 9, face = "bold"),
+          axis.text.y = element_text(size=9),
+          legend.position = "none")
 }
